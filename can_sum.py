4cf1e670a27a232fdb0de7db25073d79502be00d
Kajosz <kajetan.szczurowski@gmail.com>
2023-10-25 21:01:36 +0200

    Can sum is working fine.

--- can_sum.py
+++ can_sum.py
@@ -1,4 +1,7 @@
-def can_sum(sum_value, array, stored = {}):
+def can_sum(sum_value, array, stored = None):
+    if stored is None:
+        stored = {} 
+
     if sum_value == 0:
         return True
     if sum_value < min(array):
@@ -6,11 +9,16 @@ def can_sum(sum_value, array, stored = {}):
     if not stored.get(sum_value) is None:
         return stored[sum_value]
 
-    results = []
     for item in array:
-        results.append(can_sum(sum_value - item, array, stored))
+        if can_sum(sum_value - item, array, stored) == True:
+            stored[sum_value] = True
+            return True
     
-    stored[sum_value] = True in results
-    return stored[sum_value]
+    stored[sum_value] = False
+    return False
 
-print(can_sum(7, [2,3,4]))
+print(can_sum(7, [2,3]))
+print(can_sum(7, [5,3,4,7]))
+print(can_sum(7, [2,4]))
+print(can_sum(8, [2,3,5]))
+print(can_sum(300, [7,14]))

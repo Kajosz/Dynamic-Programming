86f018a719a28de00d1b8238104f0f1da1c9c970
Kajosz <kajetan.szczurowski@gmail.com>
2023-10-28 20:18:12 +0200

    Fixed best_sum and created grid_traveler with tabulation.

--- best_sum.py
+++ best_sum.py
@@ -1,5 +1,4 @@
 def best_sum(target, numbers, stored = None):
-    print(stored)
     if stored is None:
         stored = {}
     if target == 0:
@@ -11,7 +10,6 @@ def best_sum(target, numbers, stored = None):
         return stored[target]
     
     best_result = None
-    best_length = 42000006969
     result = []
     for item in numbers:
         diff = target - item
@@ -23,17 +21,24 @@ def best_sum(target, numbers, stored = None):
         result += [item]
         new_length = len(result)
 
+        if best_result is None:
+            best_length = new_length
+            best_result = result
+            continue
+
         if new_length < best_length:
             best_length = new_length
             best_result = result
 
-    if sum(best_result) == target:
-        print(target)
-        stored[target] = best_result     
-  
-    return best_result
+    if best_result is None:
+        stored[target] = []
+        return None
+    
+    stored[target] = list(best_result)
+    if sum(best_result) == target:     
+        return best_result
             
-# print(best_sum(8,[2,3,5]))
-# print(best_sum(300, [14,7]))
-# print(best_sum(2,[3,6,7]))
+print(best_sum(8,[2,3,5]))
+print(best_sum(300, [14,7]))
+print(best_sum(2,[3,6,7]))
 print(best_sum(8,[1,4,5]))
--- grid_traveler_table.py
+++ grid_traveler_table.py
@@ -1,5 +1,37 @@
+def create_matrix(rows, columns, placeholder = 0):
+  table = []
+  for r in range(rows):
+    new_row = []
+    for c in range(columns):
+        new_row.append(placeholder)
+    table.append(new_row)
+  return table
+
 def grid_traveler(rows, columns):
+    max_row = rows + 1
+    max_col = columns + 1
+    table = create_matrix(max_row, max_col)
+    table[1][1] = 1
+
+    for r in range(max_row):
+        if r == 0:
+            continue
+        for c in range(max_col):
+            if c == 0:
+                continue
+            if c < columns:
+                table[r][c + 1] += table[r][c]
+            if r < rows:
+                table[r + 1][c] += table[r][c]
+        
+    return table[rows][columns]
 
 
-
+print(grid_traveler(1,1))
+print(grid_traveler(2,3))
+print(grid_traveler(3,2))
 print(grid_traveler(3,3))
+print(grid_traveler(18,18))
+
+
+
